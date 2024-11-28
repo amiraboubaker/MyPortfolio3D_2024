1fce8a4a4f9d1aa882284f424ac54b80668e3dfd
Amira Boubaker <amiraboubakeresprims@gmail.com>
2024-11-28 08:15:08 +0100

    commit update

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,16 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from "./Hero";
-import Navbar from "./Navbar";
 import About from "./About";
-import Tech from "./Tech";
-import Experience from "./Experience";
-import Works from "./Works";
-import Feedbacks from "./Feedbacks";
+import { BallCanvas, ComputersCanvas, EarthCanvas, StarsCanvas } from './canvas';
 import Contact from "./Contact";
+import Experience from "./Experience";
+import Hero from "./Hero";
 import CanvasLoader from "./Loader";
+import Navbar from "./Navbar";
+import Tech from "./Tech";
+import Works from "./Works";
 
 
 export {
-  Hero,
-  Navbar,
-  About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
-  Contact,
-  CanvasLoader,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
+  About, BallCanvas, CanvasLoader, ComputersCanvas, Contact, EarthCanvas, Experience, Hero,
+  Navbar, StarsCanvas, Tech, Works
 };
+
